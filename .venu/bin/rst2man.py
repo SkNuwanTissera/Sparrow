0f2bc6cce9cdd9a35a01a9eafb01a7677d09a0ab
SkNuwanTissera <nuwan.tissera@my.sliit.lk>
2020-05-01 16:27:25 +0530

    boto3

---
+++ .venu/bin/rst2man.py
@@ -0,0 +1,26 @@
+#!/home/nuwan/Documents/Learning/Pluralsight/aws-developer-introduction-aws-lambda/pluralsight-v2-introduction-aws-lambda-master/m4/sparrow/.venu/bin/python3
+
+# Author: 
+# Contact: grubert@users.sf.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+man.py
+======
+
+This module provides a simple command line interface that uses the
+man page writer to output from ReStructuredText source.
+"""
+
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import manpage
+
+description = ("Generates plain unix manual documents.  " + default_description)
+
+publish_cmdline(writer=manpage.Writer(), description=description)
